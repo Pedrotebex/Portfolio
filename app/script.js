41b6a1eb78c89b6f8d8965320d425175de0ab0ba
Pedro Augusto Tebexreni Fraguglia Neto <106819480+Pedrotebex@users.noreply.github.com>
2024-04-25 14:41:00 -0300

    teste 2

--- app/script.js
+++ app/script.js
@@ -1,14 +1,14 @@
-// Seleção dos elementos na página Home
 // Definição dos textos em inglês
 const textsEn = {
     'idioma-pt': 'PT',
     'idioma-en': 'EN',
     'titulo': 'About Me',
     'paragrafo1': 'Hello! My name is Pedro Fraguglia and I\'m currently dedicated to studying Systems Analysis and Development at the University of São Francisco. With a passion for Front-End programming, I\'m determined to become an experienced programmer in this field.',
-    'paragrafo2': 'I have previous experience of two and a half years in the Digital Marketing industry, where I performed various functions, including social media management, creation of art and content, development of web pages using WordPress, HTML, and Javascript, as well as working with web servers. I actively participated in digital launches, contributing to exceptional results.',
-    // Adicione os textos restantes em inglês conforme necessário
+    'paragrafo2': 'I have previous experience of two and a half years in the Digital Marketing industry, where I performed various functions, including social media management, creation of art and content, development of web pages using WordPress, HTML, and Javascript, as well as working with web servers. I actively participated in digital launches, contributing to exceptional results.'
+    // Adicione mais textos conforme necessário
 };
 
+// Seleção dos elementos na página Home
 const idiomaPtBtn = document.getElementById('idioma-pt');
 const idiomaEnBtn = document.getElementById('idioma-en');
 const titulo = document.getElementById('titulo');
@@ -18,6 +18,7 @@ const paragrafo2 = document.getElementById('paragrafo2');
 
 // Função para alternar os textos entre português e inglês
 function toggleLanguage(texts) {
+    // Adiciona evento de clique ao botão de idioma PT
     idiomaPtBtn.addEventListener('click', function() {
         // Atualiza o conteúdo dos elementos para os textos em português
         titulo.textContent = texts['titulo'];
@@ -26,6 +27,7 @@ function toggleLanguage(texts) {
         // Adicione mais atribuições de texto conforme necessário
     });
 
+    // Adiciona evento de clique ao botão de idioma EN
     idiomaEnBtn.addEventListener('click', function() {
         // Atualiza o conteúdo dos elementos para os textos em inglês
         titulo.textContent = textsEn['titulo'];
